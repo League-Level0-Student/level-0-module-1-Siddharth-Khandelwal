b81dee039a45db4957f5b3c6542604572715c8e4
root <root@ec28a9ca6b12>
2020-07-09 23:56:16 +0000

    badgers

--- src/_05_for_loops/_1_my_ages/My_Ages.java
+++ src/_05_for_loops/_1_my_ages/My_Ages.java
@@ -1,7 +1,12 @@
 package _05_for_loops._1_my_ages;
 
+import javax.swing.JOptionPane;
+
 public class My_Ages {
 public static void main(String[] args) {
-	
+	for (int i = 0; i < 11; i++) {
+		JOptionPane.showMessageDialog(null,i );
+	}
 }
 }
+ 
---
+++ src/_05_for_loops/_2_badgers/Badgers.java
@@ -0,0 +1,14 @@
+package _05_for_loops._2_badgers;
+
+import javax.swing.JOptionPane;
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
+public class Badgers {
+public static void main(String[] args) {
+	for (int i = 0; i < 12; i++) {
+		System.out.println("Badger");
+	}
+		
+	}
+}
+
