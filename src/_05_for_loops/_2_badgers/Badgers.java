0c447c237c70b406381225596d794a2fb19319f6
root <root@c0120df2edf6>
2020-07-14 23:57:43 +0000

    bananas

--- src/_05_for_loops/_2_badgers/Badgers.java
+++ src/_05_for_loops/_2_badgers/Badgers.java
@@ -5,10 +5,22 @@ import javax.swing.text.StyledEditorKit.ForegroundAction;
 
 public class Badgers {
 public static void main(String[] args) {
-	for (int i = 0; i < 12; i++) {
-		System.out.println("Badger");
-	}
+	for (int j = 0; j < 2; j++) {
 		
+	
+	for (int i = 0; i < 12; i++) {
+		System.out.print("Badger ");
 	}
+		System.out.println();
+		for (int i = 0; i < 2; i++) {
+			System.out.print("Mushroom ");
+		}
+		System.out.println();
+		
+		}
+System.out.println("A Snake!!!");
 }
+}
+
+
 
---
+++ src/_05_for_loops/_4_banana_split/banana_split/banana_split.pde
@@ -0,0 +1,5 @@
+size(500,500);
+for(int i = 0; i <3;i++){
+text("Ice Cream",225,250+i*25);
+}
+text("Banana",225,325);
